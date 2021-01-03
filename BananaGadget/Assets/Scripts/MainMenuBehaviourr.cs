4e9936461ddeb21a7d911665e11dc54ff77b2455
EnderCow <luke.owen96@hotmail.com>
2021-01-03 18:21:50 +0000

    Main Menu added and done

---
+++ BananaGadget/Assets/Scripts/MainMenuBehaviourr.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehaviourr : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Cameratest");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
